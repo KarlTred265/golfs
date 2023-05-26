cd739701bb545a837e88bc0b90d3110fc5e6d220
KarlTred265 Tred <tred26559@gmail.com>
2023-05-26 17:17:43 +0000

    add structure

--- golfs/src/App.js
+++ golfs/src/App.js
@@ -1,9 +1,17 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
+    // BEM
     <div className="App">
-    <h1>Hello world wolkome</h1>
+      <h1>Hello world! Hej san</h1>
+
+      {/* Sidebar */}
+
+      {/* Feed */}
+
+      {/* Widgets */}
     </div>
   );
 }
---
+++ golfs/src/Sidebar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Sidebar.css';
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+        {/* Golf Media Icon */}
+
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+
+        {/* Button -> Golf posts */}
+    </div>
+  )
+}
+
+export default Sidebar
